6be4d0e355ef2703de17556a4e65163e41815671
sl4964 <sl4964@nyu.edu>
2017-11-02 13:59:30 -0400

    finished RRM_Sequence dataset classes

--- CNN_LSTM/RRM_Sequence.py
+++ CNN_LSTM/RRM_Sequence.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 
-class RRM_Sequence(Dataset):
+class RRM_AlignedSequence(Dataset):
 
     """RRM dataset without one-hot encoding"""
 
@@ -19,25 +19,70 @@ class RRM_Sequence(Dataset):
         self.df = pd.read_csv(info_path, index_col=0)
 
     def __len__(self):
+        # this could potentially be replaced by gene ID's 
+        # @Millie
         return len(self.names)
 
     def __getitem__(self, idx):
         rrm_name = self.names[idx]
-        
-        print(rrm_name) # DEBUGGING
-        
-        rrm = pd.read_csv(self.csv_file, index_col=0)
-
-        print(rrm.index.values) # DEBUGGING
-        print(rrm['A0A1B7NG40.1_71-102']) # DEBUGGING
-        BOOM # DEBUGGING
-        
-        rrm = rrm[rrm_name]
-        
-        rrm = torch.from_numpy(rrm).contiguous().float()
+        rrm = ''.join(self.df.loc[rrm_name, :].values)
+        rrm = '<start>' + rrm + '<end>'
         sample = {'name': rrm_name, 'seq': rrm}
-
-        if self.transform:
-            sample = self.transform(sample)
-
         return sample
+
+class RRM_OriginalSequence(Dataset):
+
+    """RRM dataset without one-hot encoding"""
+
+    def __init__(self, indices, raw_txt_path = '../data/PF00076_rp55-2.txt'):
+        """
+        Args:
+            info_path (string): path for filtered RRM sequence csv file
+            names (list of strings): RRM sequences to include
+        """
+        super(RRM_Sequence).__init__()
+        self.names = indices
+        self.df = _parse_input(raw_txt_path)
+
+    def __len__(self):
+        # this could potentially be replaced by gene ID's 
+        # @Millie
+        return len(self.names)
+
+    def __getitem__(self, idx):
+        rrm_name = self.names[idx]
+        rrm = ''.join(self.df.loc[rrm_name, :].values)
+        rrm = '<start>' + rrm + '<end>'
+        sample = {'name': rrm_name, 'seq': rrm}
+        return sample
+
+    def _parse_input(self, raw_txt_path, sep=None):
+
+        """parses txt file or fasta file into csv
+        info_positions: list of positions populated beyond a threshold"""
+
+        print('Parsing sequence input file...')
+
+        dic = dict()
+        with open(raw_txt_path) as RRM:
+            for i, line in enumerate(RRM):
+                if '#' in line:
+                    pass
+                else:
+                    name, seq = line.split(sep)
+                    name = name.replace('/', '_') # to distinguish from directory
+                    # separator down the line
+                    dic.update([(name, seq)])
+
+        df = pd.DataFrame(list(dic.values()), index=dic.keys())
+
+        return df 
+
+
+
+
+
+
+
+
+
