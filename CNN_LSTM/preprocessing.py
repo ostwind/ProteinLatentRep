4336f5b4f3aa150a2b7922ec0a9cb2697b02f503
Julie Helmers <jchelmers@nyu.edu>
2017-11-15 16:46:20 -0500

    Finished updates to use new combined data

--- CNN_LSTM/preprocessing.py
+++ CNN_LSTM/preprocessing.py
@@ -10,7 +10,6 @@ import pickle
 from build_vocab import build_vocab
 from sklearn.preprocessing import LabelEncoder
 
-
 def txt_to_csv(raw_txt_path, sep=None):
     """parses txt file or fasta file into csv
     info_positions: list of positions populated beyond a threshold"""
@@ -18,39 +17,57 @@ def txt_to_csv(raw_txt_path, sep=None):
     print('Parsing sequence input file...')
 
     dic = dict()
+    name = None
+    seq = None
     with open(raw_txt_path) as RRM:
+#        # UNCOMBINED DATA
+#        for i, line in enumerate(RRM):
+#            if '#' in line:
+#                pass
+#            else:
+#                name, seq = line.split(sep)
+#                name = name.replace('/', '_') # to distinguish from directory
+#                # separator down the line
+#                dic.update([(name, seq)])
+
+        # COMBINED DATA
         for i, line in enumerate(RRM):
             if '#' in line:
                 pass
-            else:
-                name, seq = line.split(sep)
+            elif '>' in line:
+                name = line[:-1] # ignore newline char at end
+                name = name.replace('>', '') # to get rid of unnecessary symbols
                 name = name.replace('/', '_') # to distinguish from directory
-                # separator down the line
+            else:
+                seq = line[:-1] # ignore newline char at end
+                
+            if name and seq:
                 dic.update([(name, seq)])
-
-        df = pd.DataFrame(list(map(lambda x: list(x.upper()), 
+                name = None
+                seq = None
+    
+        df = pd.DataFrame(list(map(lambda x: list(x.upper()),
             list(dic.values()))), index=dic.keys())
     return df
 
-
-def informative_positions(df, processed_RRM_path, top_n, placeholder='-'):
+def informative_positions(df, processed_RRM_path, top_n=82, placeholder='-'):
     """rid of excessive placeholders, 
     keeping top_n most populated positions
     for pfam dataset, this is equivalent to keeping populated rate @.001 """
 
     print('Extracting top %d most populated positions...'%top_n)
 
-    populate_rate = df.applymap(lambda x: x != placeholder).sum(axis=0)/\
-    df.applymap(lambda x: 1 if x else 0).sum(axis=0)
+    populate_rate = df.applymap(lambda x: x != placeholder).sum(axis=0) / \
+                    df.applymap(lambda x: 1 if x else 0).sum(axis=0)
     positions_to_keep = [i for i, rate in enumerate(populate_rate) \
-    if rate in sorted(populate_rate, reverse=True)[:top_n]]
+                         if rate in sorted(populate_rate, reverse=True)[:top_n]]
     informative_values = list(map(lambda x: ['<start>'] + x.tolist() + ['<end>'], 
-        df[positions_to_keep].values))
+                                  df[positions_to_keep].values))
     df1 = pd.DataFrame(informative_values, index=df.index)
     df1.to_csv(processed_RRM_path)
     return df1
 
-def preprocess(preprocessed=False, RRM_path, output_path):
+def preprocess(preprocessed, RRM_path, output_path):
     """if aligned=False, a vocab should be passed"""
     
     assert os.path.isfile(RRM_path), 'input RRM path: %s not found!' %(RRM_path)
--- CNN_LSTM/train_test_split.py
+++ CNN_LSTM/train_test_split.py
@@ -5,12 +5,13 @@ import pandas as pd
 import numpy as np
 
 np.random.seed(0)
-ind_df = pd.read_csv('../data/combined_data.txt') #('../data/rrm_rp55.csv')
-msk = np.random.randn(ind_df.shape[0]) < .7
+#ind_df = pd.read_csv('../data/rrm_rp55.csv')  # UNCOMBINED DATA
+ind_df = pd.read_csv('../data/aligned_processed_RRM.csv')  # COMBINED DATA
+msk = np.random.rand(ind_df.shape[0]) < .7
 ind_df[msk].iloc[:,0].to_csv('../data/train_index.csv', 
 	index=False, header=False)
 test = ind_df[~msk]
-test_msk = np.random.randn(test.shape[0]) < 1/3
+test_msk = np.random.rand(test.shape[0]) < 1.0/3
 test[test_msk].iloc[:,0].to_csv('../data/test_index.csv', 
 	index=False, header=False)
 test[~test_msk].iloc[:,0].to_csv('../data/val_index.csv', 
