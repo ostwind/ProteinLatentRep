24f0119f4e978c5dfb1bdab96944b824222851b2
Julie Helmers <jchelmers@nyu.edu>
2017-11-15 11:19:11 -0500

    Debug slurm script

--- CNN_LSTM/train_decoder.s
+++ CNN_LSTM/train_decoder.s
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 #SBATCH --gres=gpu:1
-#SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8GB
-#SBATCH --ntasks=1
+#SBATCH --nodes=1
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jch609@nyu.edu
-#SBATCH --output=slurm_%j.out
+#SBATCH --output=../output/slurm_%j.out
 
 module purge
-module load pytorch/intel/20170226
 module load python3/intel/3.5.3
-module load torchvision/0.1.8
+module load pytorch/python3.5/0.2.0_3
 
 python train_decoder.py
