78012e6b38f2795771263d59032cca161f167e72
Julie Helmers <jchelmers@nyu.edu>
2017-11-15 20:43:47 -0500

    Currently running on prince

--- CNN_LSTM/train_decoder.s
+++ CNN_LSTM/train_decoder.s
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8GB
+#SBATCH --mem=16GB
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
@@ -13,6 +13,7 @@
 module purge
 module load python3/intel/3.5.3
 module load pytorch/python3.5/0.2.0_3
+module load torchvision/python3.5/0.1.9
 
-python train_decoder.py
-#python train_decoder.py --preprocessed --aligned_RRM_sequence "../data/aligned_processed_RRM.csv"
+#python3 train_decoder.py
+python3 train_decoder.py --preprocessed --aligned_RRM_path="../data/aligned_processed_RRM.csv"
