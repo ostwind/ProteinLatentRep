3be86ff322c5a71f3bf9731a5dc4f4fb4f16b62e
sl4964 <sl4964@nyu.edu>
2017-12-05 20:04:01 -0500

    making tsne prettier

--- util/tsne.py
+++ util/tsne.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np 
 
-def tsne(matrix,
-      perplexity = 40, n_iter = 2000, n_components = 2, ): 
+def tsne(matrix, perplexity = 40, n_iter = 2000, n_components = 2): 
       tsne = TSNE(
             n_components= n_components, verbose=1, perplexity=perplexity, n_iter=n_iter)
       return tsne.fit_transform(matrix)
 
-def plot3d(plot_name, labels, latent_representation, take_first_n = 10000): 
+def plot3d(plot_name, labels, latent_representation, take_first_n = 10000, 
+      perplexity = 40, n_iter = 2000, n_components = 2): 
       ''' list int, str labels: color point according to this color 
           np.ndarray tsne_projection: [ Num of sequences X Dim of Latent Rep. ]
           take_first_n: only first n row will show in tsne projection 
@@ -34,12 +34,12 @@ def plot3d(plot_name, labels, latent_representation, take_first_n = 10000):
       unique_labels_filtered = []
       for l in unique_labels: 
             all_l_indices = [i for i,x in enumerate(labels) if x == l]
-            if len(all_l_indices) < 40:
+            if len(all_l_indices) < 20:
                   continue  
             large_gene_symbol.append(all_l_indices)
             unique_labels_filtered.append(l)
 
-      fig = plt.figure()
+      fig = plt.figure(figsize=(10, 8))
       ax = fig.add_subplot(111, projection='3d')
 
       colors = iter(cm.jet(np.linspace(0, 1, len(large_gene_symbol))))
@@ -49,19 +49,22 @@ def plot3d(plot_name, labels, latent_representation, take_first_n = 10000):
                   tsne_projection[all_l_indices,0], 
                   tsne_projection[all_l_indices,1],
                   tsne_projection[all_l_indices,2], 
-                  c= cur_color, label=l, s = 5)
+                  c= cur_color, label=l, s = 30)
 
-      ax.legend()
+      ax.legend(fontsize=15)
       #ax = plt.gca()
       #legend = ax.get_legend()
       # ax.set_zlim(-10, 10)
       # ax.set_ylim(-10, 10)
+      plt.title(plot_name, fontsize=18)
       # ax.set_xlim(-10, 10)
       plt.savefig(plot_name, bbox_inches='tight')
       plt.show()
+      return fig
 
 
-def plot(plot_name, labels, latent_representation, take_first_n = 10000): 
+def plot(plot_name, labels, latent_representation, take_first_n = 10000, 
+      n_iters=2000, perplexity=40): 
       ''' list int, str labels: color point according to this color 
           np.ndarray tsne_projection: [ Num of sequences X Dim of Latent Rep. ]
           take_first_n: only first n row will show in tsne projection 
@@ -86,20 +89,23 @@ def plot(plot_name, labels, latent_representation, take_first_n = 10000):
       for l in unique_labels: 
             all_l_indices = [i for i,x in enumerate(labels) if x == l]
             if len(all_l_indices) < 40:
-                  continue  
+                  continue
             large_gene_symbol.append(all_l_indices)
             unique_labels_filtered.append(l)
 
+      fig = plt.figure(figsize=(20, 20))
+      ax = fig.add_subplot(111)
       colors = iter(cm.jet(np.linspace(0, 1, len(large_gene_symbol))))
       for l, all_l_indices in zip(unique_labels_filtered, large_gene_symbol):
-            cur_color = next(colors)
+            
+            cur_color = next(colors)            
             plt.scatter(
                   tsne_projection[all_l_indices,0], 
                   tsne_projection[all_l_indices,1], 
-                  c= cur_color, label=l, s = 5)
+                  c= cur_color, label=l, s = 30)
 
-      plt.legend()
-      ax = plt.gca()
+      ax.legend(fontsize=16)
+      plt.title(plot_name, fontsize=25)
       #legend = ax.get_legend()
       
       plt.savefig(plot_name, bbox_inches='tight')
