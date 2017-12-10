cd46ebc4897c861c90c2ec0cb38f4a916e888d96
sl4964 <sl4964@nyu.edu>
2017-12-09 22:09:20 -0500

    debug encoder_rep.py

--- CNN_LSTM/encoder_rep.py
+++ CNN_LSTM/encoder_rep.py
@@ -4,10 +4,11 @@ from EncoderCNN import ResNetEncoder
 import torch
 
 combined_RRMs = pd.read_csv('combined_processed_RRM.csv', index_col=0)
-# if using CPU
-encoder = torch.load('encoder-anneal-True-22-1000.pkl', map_location=lambda storage, loc: storage)
-# use line below if running on GPU
-# encoder = torch.load('encoder-anneal-True-22-1000.pkl)
+
+if not torch.cuda.is_available():
+	encoder = torch.load('encoder-anneal-True-22-1000.pkl', map_location=lambda storage, loc: storage)
+else:
+	encoder = torch.load('encoder-anneal-True-22-1000.pkl')
 
 
 vocab = pickle.load(open('vocab2.pkl', 'rb'))
@@ -17,9 +18,22 @@ combined_RRMs_input = pd.DataFrame(list(map(lambda word: [vocab(x) for x in word
 combined_RRMs_input.index = combined_RRMs.index
 
 new_encoder = ResNetEncoder(84,26, 64)
-new_encoder.load_state_dict(encoder)
-print('we crunching...')
-combined_RRMs_output = new_encoder(torch.autograd.Variable(torch.Tensor(combined_RRMs_input.values.astype(float)).long()))
+if torch.cuda.is_available():
+	new_encoder = new_encoder.cuda()
 
-# line below is not tested (may need tweaking)
-pd.DataFrame(combined_RRMs_output.data.numpy()).to_csv('encoder_rep.csv')
+new_encoder.load_state_dict(encoder)
+
+print('we crunching...')
+
+if not torch.cuda.is_available():
+	combined_RRMs_output = new_encoder(torch.autograd.Variable(torch.Tensor(combined_RRMs_input.values.astype(float)).long()))
+else:
+	combined_RRMs_output = new_encoder(torch.autograd.Variable(torch.Tensor(combined_RRMs_input.values.astype(float)).long()).cuda())
+
+# code below is not tested (may need tweaking)
+if not torch.cuda.is_available():
+	combined_RRMs_output = pd.DataFrame(combined_RRMs_output.data.numpy())
+else:
+	combined_RRMs_output = pd.DataFrame(combined_RRMs_output.data.cpu().numpy())
+combined_RRMs_output.index = combined_RRMs.index
+combined_RRMs_output.to_csv('encoder_rep.csv')
