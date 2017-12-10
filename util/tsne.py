d4a5e7ea075680c5161cbea715c8eea39977faed
sl4964 <sl4964@nyu.edu>
2017-12-10 01:21:10 -0500

    bug fix in util/tsne.py

--- util/tsne.py
+++ util/tsne.py
@@ -11,103 +11,103 @@ def tsne(matrix,
       return tsne.fit_transform(matrix)
 
 
-import plotly.plotly as py
-import plotly.graph_objs as go
-import plotly
+# import plotly.plotly as py
+# import plotly.graph_objs as go
+# import plotly
 import random
 
-def plotly_scatter(plot_name, labels, latent_representation, take_first_n = 10000):
-      plotly.tools.set_credentials_file(username='mrnood', api_key='2o0NGhHfo4RXCJudQ3Mi')
+# def plotly_scatter(plot_name, labels, latent_representation, take_first_n = 10000):
+#       plotly.tools.set_credentials_file(username='mrnood', api_key='2o0NGhHfo4RXCJudQ3Mi')
       
-      if take_first_n > len(labels):
-            take_first_n = len(labels)
+#       if take_first_n > len(labels):
+#             take_first_n = len(labels)
 
-      labels = labels[:take_first_n]
-      latent_representation = latent_representation[:take_first_n, :]
-      latent_representation = tsne(latent_representation)
+#       labels = labels[:take_first_n]
+#       latent_representation = latent_representation[:take_first_n, :]
+#       latent_representation = tsne(latent_representation)
 
-      unique_labels = sorted(list(set(labels)))
-      #unique_labels = [l for l in unique_labels if l[0] != 'S'] # autencoder didnt work here, removing 'noise' atm
-      # plot only gene symbols with >40 RRMs
-      large_gene_symbol, unique_labels_filtered = [], []
-      for l in unique_labels: 
-            all_l_indices = [i for i,x in enumerate(labels) if x == l]
-            if len(all_l_indices) < 40 or len(all_l_indices) > 60:
-                  continue  
-            print(l, len(all_l_indices))
-            large_gene_symbol.append(all_l_indices)
-            unique_labels_filtered.append(l)
+#       unique_labels = sorted(list(set(labels)))
+#       #unique_labels = [l for l in unique_labels if l[0] != 'S'] # autencoder didnt work here, removing 'noise' atm
+#       # plot only gene symbols with >40 RRMs
+#       large_gene_symbol, unique_labels_filtered = [], []
+#       for l in unique_labels: 
+#             all_l_indices = [i for i,x in enumerate(labels) if x == l]
+#             if len(all_l_indices) < 40 or len(all_l_indices) > 60:
+#                   continue  
+#             print(l, len(all_l_indices))
+#             large_gene_symbol.append(all_l_indices)
+#             unique_labels_filtered.append(l)
 
-      large_gene_symbol = np.array(large_gene_symbol)
+#       large_gene_symbol = np.array(large_gene_symbol)
 
-      l = []
-      N= len(unique_labels_filtered)
-      c= ['hsl('+str(h)+',50%'+',40%)' for h in np.linspace(0, 360, N)]
-      c2 = ['hsl('+str(h)+',50%'+',30%)' for h in np.linspace(0, 360, N)]
-      c3 = ['hsl('+str(h)+',40%'+',60%)' for h in np.linspace(0, 360, N)]
+#       l = []
+#       N= len(unique_labels_filtered)
+#       c= ['hsl('+str(h)+',50%'+',40%)' for h in np.linspace(0, 360, N)]
+#       c2 = ['hsl('+str(h)+',50%'+',30%)' for h in np.linspace(0, 360, N)]
+#       c3 = ['hsl('+str(h)+',40%'+',60%)' for h in np.linspace(0, 360, N)]
       
-      palette = [c2, c, c3]
-      c = 0
-      for unique_label, all_l_indices in zip(unique_labels_filtered, large_gene_symbol):
-            cur_palette = c % 3
-            #y.append((2000+i))
-            #for point in all_l_indices:
-            print(palette[cur_palette][c])
-            trace0 = go.Scatter3d(
-                  x=latent_representation[all_l_indices, 0],
-                  y=latent_representation[all_l_indices, 1],
-                  z=latent_representation[all_l_indices, 2],
-                  mode='markers',
-                  marker=dict(size=6,
-                              #colorscale='jet',  # 'Viridis',
-                              #line= dict(width=1),
-                              color=palette[cur_palette][c],
-                              #[unique_labels_filtered.index(unique_label)],
-                              #opacity=0.75,
-                              ), name=unique_label,)
-            #text= unique_label) # The hover text goes here...
-            l.append(trace0)
-            c += 1
-      layout = go.Layout(
-                    scene = dict(
-                    xaxis = dict(
-                        #nticks=4,
-                        #gridwidth=4,
-                        title = '',
-                        showgrid=False,
-                        showline=False,
-                        #zeroline=False,
-                        showticklabels=False,
-                         ),
-                    yaxis = dict(
-                        #nticks=4,
-                        title = '',
-                        showgrid=False,
-                        #gridwidth=2,
-                        showline=False,
-                        #zeroline=False,
-                        showticklabels=False,
-                         ),
-                    zaxis = dict(
-                        #nticks=4,
-                        title = '', 
-                        gridwidth=2,
-                        showgrid = False,
-                        showline=False,
-                        #zeroline=False,
-                        showticklabels=False,
-                        ),
-                        ),
-                  height = 1000,
-                  width=1200,
-                  margin=dict(
-                  r=0, l=0,
-                  b=0, t=0),
-                  showlegend= True,
-                  )
+#       palette = [c2, c, c3]
+#       c = 0
+#       for unique_label, all_l_indices in zip(unique_labels_filtered, large_gene_symbol):
+#             cur_palette = c % 3
+#             #y.append((2000+i))
+#             #for point in all_l_indices:
+#             print(palette[cur_palette][c])
+#             trace0 = go.Scatter3d(
+#                   x=latent_representation[all_l_indices, 0],
+#                   y=latent_representation[all_l_indices, 1],
+#                   z=latent_representation[all_l_indices, 2],
+#                   mode='markers',
+#                   marker=dict(size=6,
+#                               #colorscale='jet',  # 'Viridis',
+#                               #line= dict(width=1),
+#                               color=palette[cur_palette][c],
+#                               #[unique_labels_filtered.index(unique_label)],
+#                               #opacity=0.75,
+#                               ), name=unique_label,)
+#             #text= unique_label) # The hover text goes here...
+#             l.append(trace0)
+#             c += 1
+#       layout = go.Layout(
+#                     scene = dict(
+#                     xaxis = dict(
+#                         #nticks=4,
+#                         #gridwidth=4,
+#                         title = '',
+#                         showgrid=False,
+#                         showline=False,
+#                         #zeroline=False,
+#                         showticklabels=False,
+#                          ),
+#                     yaxis = dict(
+#                         #nticks=4,
+#                         title = '',
+#                         showgrid=False,
+#                         #gridwidth=2,
+#                         showline=False,
+#                         #zeroline=False,
+#                         showticklabels=False,
+#                          ),
+#                     zaxis = dict(
+#                         #nticks=4,
+#                         title = '', 
+#                         gridwidth=2,
+#                         showgrid = False,
+#                         showline=False,
+#                         #zeroline=False,
+#                         showticklabels=False,
+#                         ),
+#                         ),
+#                   height = 1000,
+#                   width=1200,
+#                   margin=dict(
+#                   r=0, l=0,
+#                   b=0, t=0),
+#                   showlegend= True,
+#                   )
 
-      fig= go.Figure(data=l, layout=layout)
-      py.iplot(fig)
+#       fig= go.Figure(data=l, layout=layout)
+#       py.iplot(fig)
 
 
 def plot3d(plot_name, labels, latent_representation, take_first_n = 10000, 
@@ -149,9 +149,8 @@ def plot3d(plot_name, labels, latent_representation, take_first_n = 10000,
       colors = iter(cm.jet(np.linspace(0, 1, len(large_gene_symbol))))
       for l, all_l_indices in zip(unique_labels_filtered, large_gene_symbol):
             cur_color = next(colors)
-            plt.scatter(
+            ax.scatter(
                   tsne_projection[all_l_indices,0], 
-
                   tsne_projection[all_l_indices,1],
                   tsne_projection[all_l_indices,2], 
                   c= cur_color, label=l, s = 30)
@@ -207,7 +206,7 @@ def plot(plot_name, labels, latent_representation, take_first_n = 10000,
       unique_labels_filtered = []
       for l in unique_labels: 
             all_l_indices = [i for i,x in enumerate(labels) if x == l]
-            if len(all_l_indices) < 40:
+            if len(all_l_indices) < 20:
                   continue
             large_gene_symbol.append(all_l_indices)
             unique_labels_filtered.append(l)
@@ -221,7 +220,7 @@ def plot(plot_name, labels, latent_representation, take_first_n = 10000,
             plt.scatter(
                   tsne_projection[all_l_indices,0], 
                   tsne_projection[all_l_indices,1], 
-                  c= cur_color, label=l, s = 30)
+                  c= cur_color, label=l, s = 50)
 
       ax.legend(fontsize=16)
       plt.title(plot_name, fontsize=25)
