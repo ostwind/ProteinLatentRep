341abc3502458c632a260b35bc6bad8766dcd788
Julie Helmers <jchelmers@nyu.edu>
2017-12-10 21:00:37 -0500

    Update default options, clarify comments
    Use get_hidden.py instead of encoder_rep.py

--- CNN_LSTM/get_hidden.s
+++ CNN_LSTM/get_hidden.s
@@ -8,11 +8,13 @@
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jch609@nyu.edu
-#SBATCH --output=../output/slurm_%j.out
+#SBATCH --output=../output/hidden_%j.out
+#SBATCH --error=../output/hidden_%A.err
 
 module purge
 module load python3/intel/3.5.3
 module load pytorch/python3.5/0.2.0_3
 module load torchvision/python3.5/0.1.9
 
-python3 get_hidden.py
+#python3 get_hidden.py --which_representation 'decoder' --hidden_path './TrainedModels_48hr/encoder_reps_aligned_48hrs.csv'
+python3 get_hidden.py --which_representation 'encoder'  # default
